e58569be8318a70c24819a21eeb2fe2e7ae9bb7b
seoin0529 <59160842+seoin0529@users.noreply.github.com>
2020-01-19 07:26:42 +0900

    Add files via upload

--- js/portfolio.js
+++ js/portfolio.js
@@ -1,26 +1,55 @@
 
 
 $("document").ready(function(){
+	
 	var typingBool = false; 
-    var typingIdx=0;
-    var typingTxt = $(".section_wrap .home .txt-wrap .txt-typing ").text(); // 타이핑될 텍스트를 가져온다 
-    typingTxt=typingTxt.split(""); // 한글자씩 자른다. 
-    if(typingBool==false){ // 타이핑이 진행되지 않았다면 
-       typingBool=true; 
-       
-       var tyInt = setInterval(typing,100); // 반복동작 
-     } 
+var typingIdx=0; 
+var liIndex = 0;
+var liLength = $(".section_wrap .home .txt-wrap ul li").length;
+
+// 타이핑될 텍스트를 가져온다 
+var typingTxt = $(".section_wrap .home .txt-wrap ul li").eq(liIndex).text(); 
+typingTxt=typingTxt.split(""); // 한글자씩 자른다. 
+if(typingBool==false){ // 타이핑이 진행되지 않았다면 
+    typingBool=true; 
+    var tyInt = setInterval(typing,100); // 반복동작 
+} 
      
-     function typing(){ 
-       if(typingIdx<typingTxt.length){ // 타이핑될 텍스트 길이만큼 반복 
-         $(".section_wrap .home .txt-wrap .typing").append(typingTxt[typingIdx]); // 한글자씩 이어준다. 
-         typingIdx++; 
-       } else{ 
-         clearInterval(tyInt); //끝나면 반복종료 
-       } 
-     }  
+function typing(){ 
+  $(".typing ul li").removeClass("on");
+   $(".typing ul li").eq(liIndex).addClass("on");
+  if(typingIdx<typingTxt.length){ // 타이핑될 텍스트 길이만큼 반복 
+     $(".typing ul li").eq(liIndex).append(typingTxt[typingIdx]); // 한글자씩 이어준다. 
+     typingIdx++; 
+   } else{ if(liIndex<liLength-1){
+     //다음문장으로  가기위해 인덱스를 1증가
+       liIndex++; 
+     //다음문장을 타이핑하기위한 셋팅
+        typingIdx=0;
+        typingBool = false; 
+        typingTxt = $(".section_wrap .home .txt-wrap ul li").eq(liIndex).text(); 
+       
+     //다음문장 타이핑전 1초 쉰다
+         clearInterval(tyInt);
+          //타이핑종료
+     
+         setTimeout(function(){
+           //1초후에 다시 타이핑 반복 시작
+           tyInt = setInterval(typing,100);
+         },1000);
+        } else if(liIndex==liLength-1){
+          
+         //마지막 문장까지 써지면 반복종료
+           clearInterval(tyInt);
+        }
+    } 
+}  
+
 	
-	
+$(".home").ripples({
+	resolution : 256,
+	perturbance : 0.04
+})	
 	
 	
 	var $indi =$(".indicator ul li"),
@@ -220,7 +249,7 @@ var $btn_m =$(".section_wrap .btn.m span"),
 	pc_url=[" https://seoin0529.github.io/atwosomeplace/",
 	"https://seoin0529.github.io/Samsonite/",
 	"https://seoin0529.github.io/mobile_MCM/",
-			"https://seoin0529.github.io/mobile_MCM/"]
+			"http://westai.co.kr/dior.html"]
 	$btn_m.click(function(){
 		var i = $btn_m.index(this);
 		window.open(pc_url[i],"KIMSEOIN'S PORTFOLO","width=360px,height=640px")
